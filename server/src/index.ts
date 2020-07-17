994c52c8f33f83b22a690d14d79fad9bfb977726
Michael Shokoya <michaelshokoya@Michaels-MacBook-Pro.local>
2020-07-17 16:52:57 +0100

    server init

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,17 +3,17 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 
-// const port = process.env.PORT
+const port = process.env.PORT
 
 const mount = async (app: Application) => {
   app.use(bodyParser.json({ limit: "2mb" }));
-  // app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(cookieParser(process.env.COOKIE_SECRET));
   app.use(compression());
 
   app.get('/', (req, res) => res.send('hello world'));
 
-  app.listen(4000, () => {
-    console.log('listening on port 4000')
+  app.listen(port, () => {
+    console.log('listening on port 4000');
   });
 
 }
